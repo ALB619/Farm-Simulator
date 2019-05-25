e8ecc3bad663e04bef22286e08311e41a46d7923
ALB619 <47913534+ALB619@users.noreply.github.com>
2019-05-24 19:53:11 -0700

    Create Triceratops.java

---
+++ Triceratops.java
@@ -0,0 +1,116 @@
+package cs143.axlal;
+
+import java.util.Random;
+
+public class Triceratops extends Dinosaur{
+	
+	//Dinosaur 3
+	
+	private boolean sleep = false;
+	
+	private int hunger = 0;
+	
+	private String sound = "Raww ";
+	
+	private String name;
+	
+	private String type = "Triceratops";
+
+	public Triceratops(int hunger, String name) {
+		super(hunger, name);
+		this.hunger = hunger;
+		this.name = name;
+	}
+
+	@Override
+	public StringBuilder speak() {
+		
+		StringBuilder speak = new StringBuilder();
+		
+		if(isAsleep())
+		{
+			speak.append("snore");
+		}
+		else
+		{
+			int amountSpeaks = (getHungerLevel1() + 1) * 2;
+		
+			for(int i = 0; i < amountSpeaks; i++)
+			{
+				speak.append(sound);
+			}
+		}
+
+		return speak;
+	
+	}
+
+	@Override
+	public void feed() {
+		
+		if(isAsleep() == false)
+		{
+			if(hunger > 4)
+			{
+				hunger = hunger - 5;
+			
+				Random rand = new Random();
+		
+				int slp = rand.nextInt(10);
+		
+				if(slp == 1)
+				{
+					sleep = true;
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public String getName() {
+		
+		return name;
+	}
+
+	
+	public int getHunger() {
+		
+		return hunger;
+	}
+
+	public String getType() {
+		
+		return type;
+	}
+
+	@Override
+	public void tick() 
+	{
+		if(hunger < 24)
+		{
+			hunger = hunger + 1;
+		}
+		sleep = false;
+		
+	}
+	
+	public boolean isAsleep()	{
+		
+		if(sleep == true)
+		{
+			return true;
+		}
+		else return false;
+	}
+	
+	public boolean ifFull()	{
+		
+		if(hunger < 5)
+		{
+			return true;
+		}
+		else return false;
+	}
+	
+}
